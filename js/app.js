5eb8425e3d7e03e27883a00626163d76db57f573
YL Ang <skya.ayl@gmail.com>
2025-07-11 16:21:08 +0800

    WIP - Updated new game menu

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ let [board, currPlayer] = setUpBoard(10);
 // const resetButton = document.querySelector("#reset");
 
 /* ------------------------------ Cached Reference Elements ------------------------------ */
+const nav = document.querySelector("#title");
+const newGameMenu = document.querySelector("#new-game-menu");
+
 /* -------------------------------------- Functions -------------------------------------- */
 /* ----------------------------------- Event Listeners ----------------------------------- */
 game.addEventListener("click", (event) => {
@@ -14,6 +17,16 @@ game.addEventListener("click", (event) => {
   }
 });
 // updateBoardDisplay(board);
+
+nav.addEventListener("click", (event) => {
+  console.log(`event is running`);
+  if (event.target.innerText === "New Game") {
+    console.log(newGameMenu.style.display);
+    newGameMenu.style.display === "block"
+      ? (newGameMenu.style.display = "none")
+      : (newGameMenu.style.display = "block");
+  }
+});
 
 // resetButton.addEventListener("click", (target) => {
 //   [board, currPlayer] = resetGame(board);
